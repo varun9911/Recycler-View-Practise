8463b815f370e7fe6ed16db41a447e2229038ff6
vishusahab <rockyvarun42@gmail.com>
2020-06-25 00:53:58 +0530

    Made minor updates

---
+++ app/src/main/java/net/varun/Movie.kt
@@ -0,0 +1,18 @@
+package net.varun
+
+import com.google.gson.annotations.SerializedName
+
+data class Movie(
+    val id: Int,
+    val image: String,
+    @SerializedName("is_new")
+    val isNew: Int,
+    val rating: String,
+    @SerializedName("like_percent")
+    val likePercent: Int,
+    @SerializedName("vote_count")
+    val voteCount: Int,
+    val title:String,
+    val language:String,
+    val type: String
+)
